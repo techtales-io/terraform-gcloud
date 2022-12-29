c28a8d9a757c5c3b0a013626e7effa25cc842001
Jazzlyn <9011011+jazzlyn@users.noreply.github.com>
2022-12-30 00:36:13 +0100

    fix(ci): update sops and permissions

--- google/project_iam/locals.tf
+++ google/project_iam/locals.tf
@@ -9,11 +9,12 @@ locals {
     # github action for gcloud infra
     "serviceAccount:github-gcloud-infra${local.serviceaccount_suffix}" = [
       "roles/cloudkms.admin",
-      "roles/run.developer",
-      "roles/iam.workloadIdentityPoolAdmin",
-      "roles/resourcemanager.projectIamAdmin",
       "roles/iam.serviceAccountAdmin",
       "roles/iam.serviceAccountKeyAdmin",
+      "roles/iam.workloadIdentityPoolAdmin",
+      "roles/resourcemanager.projectIamAdmin",
+      "roles/run.developer",
+      "roles/serviceusage.serviceUsageViewer",
     ]
   }
 
