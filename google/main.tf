3897f45ad0eb7dcfe9e1547a4ee2806c9bb8754e
Jazzlyn <9011011+jazzlyn@users.noreply.github.com>
2022-12-29 23:19:06 +0100

    chore: rework structure and update versions

--- google/main.tf
+++ google/main.tf
@@ -3,6 +3,15 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.42.1"
+    }
+  }
+}
+
 module "api_services" {
   source     = "./api_services"
   project_id = var.project_id
