e3b5e8612bc9b8ad3ea53e8260f6c9a1896b80c6
Jazzlyn <9011011+jazzlyn@users.noreply.github.com>
2023-02-19 00:11:23 +0100

    fix(vault): add kms viewer for vault in new version 1.12

--- google/service_accounts/vault_unseal.tf
+++ google/service_accounts/vault_unseal.tf
@@ -17,8 +17,14 @@ resource "google_kms_crypto_key" "vault_unseal" {
 }
 
 # bind service account to kms key
-resource "google_kms_crypto_key_iam_member" "vault_unseal_decrypt" {
+resource "google_kms_crypto_key_iam_member" "vault_unseal_kms_encryptdecrypt" {
   crypto_key_id = google_kms_crypto_key.vault_unseal.id
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+  member        = "serviceAccount:${google_service_account.vault_unseal.email}"
+}
+
+resource "google_kms_crypto_key_iam_member" "vault_unseal_kms_viewer" {
+  crypto_key_id = google_kms_crypto_key.vault_unseal.id
+  role          = "roles/cloudkms.viewer"
   member        = "serviceAccount:${google_service_account.vault_unseal.email}"
 }
